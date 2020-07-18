cbb24e07ac2a1a14f9712268a26ddc6c4da3bff1
Jochen Oonincx <jochenoonincx@shadow-corporation.com>
2020-07-18 18:34:17 +0200

    Updated the example to use the new image version.

--- cmangos_realmd/docker-run.sh
+++ cmangos_realmd/docker-run.sh
@@ -6,4 +6,4 @@ sudo docker run --detach \
   --mount "type=volume,source=cmangos_realmd_etc,destination=/opt/cmangos/etc,readonly=false" \
   --publish 3724:3724 \
   --restart unless-stopped \
- cmangos_realmd:0.1
+ cmangos_realmd:0.2
