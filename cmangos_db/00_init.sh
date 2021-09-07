457898437b39f50fb0c10413889fc040dcaa3127
Jochen Oonincx <jochenoonincx@shadow-corporation.com>
2021-09-07 16:49:43 +0200

    Working on implementing the Commit builds.
    Signed-off-by: Jochen Oonincx <jochenoonincx@shadow-corporation.com>

--- cmangos_db/00_init.sh
+++ cmangos_db/00_init.sh
@@ -9,7 +9,17 @@ mysql -uroot -pmangos -e "grant all privileges on wotlkcharacters.* to 'mangos'@
 mysql -uroot -pmangos -e "grant all privileges on wotlkmangos.* to 'mangos'@'%';"
 mysql -uroot -pmangos -e "grant all privileges on wotlkrealmd.* to 'mangos'@'%';"
 git clone https://github.com/cmangos/mangos-wotlk.git /tmp/cmangos
+if [ "$CORE_COMMIT_HASH" != "HEAD" ]; then
+  echo -e "Switching to Core Commit: ${CORE_COMMIT_HASH}\n"
+  cd /tmp/cmangos
+  git checkout ${CORE_COMMIT_HASH}
+fi
 git clone https://github.com/cmangos/wotlk-db /tmp/db
+if [ "$DB_COMMIT_HASH" != "HEAD" ]; then
+  echo -e "Switching to DB Commit: ${DB_COMMIT_HASH}\n"
+  cd /tmp/db
+  git checkout ${DB_COMMIT_HASH}
+fi
 mysql -uroot -pmangos wotlkcharacters < /tmp/cmangos/sql/base/characters.sql
 mysql -uroot -pmangos wotlkrealmd < /tmp/cmangos/sql/base/realmd.sql
 cp -v /docker-entrypoint-initdb.d/InstallFullDB.config /tmp/db/InstallFullDB.config
